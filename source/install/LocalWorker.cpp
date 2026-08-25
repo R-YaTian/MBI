f66ff97ee544b4bf1bfafa07570e91fcabf39ddd
R-YaTian <darkbluesky@outlook.com>
2026-08-25 22:35:21 +0800

    Use devkita64:20260202

--- source/install/LocalWorker.cpp
+++ source/install/LocalWorker.cpp
@@ -8,7 +8,7 @@ namespace app::install
     {
         m_storageSrc = storageSrc;
         m_file = fopen(path.c_str(), "rb");
-        if (!m_file)
+        if (m_file == nullptr)
         {
             THROW_FORMAT("can't open file at %s\n", path.c_str());
         }
@@ -25,11 +25,11 @@ namespace app::install
 
     void LocalWorker::StreamToPlaceholder(std::shared_ptr<nx::ncm::ContentStorage>& contentStorage, NcmContentId ncaId, nx::nca::NcaHeader* header)
     {
-        if (m_storageSrc == LocalStorageSource::UDISK)
-        {
-            this->WriteToPlaceholderBuffered(contentStorage, ncaId, (void *)this, header, appletGetAppletType() == AppletType_LibraryApplet ? 8 : 128);
-        }
-        else
+        // if (m_storageSrc == LocalStorageSource::UDISK)
+        // {
+        //     this->WriteToPlaceholderBuffered(contentStorage, ncaId, (void *)this, header, appletGetAppletType() == AppletType_LibraryApplet ? 8 : 128);
+        // }
+        // else
         {
             this->WriteToPlaceholderDirectly(contentStorage, ncaId, DEFAULT_READ_BUFFER_SIZE, header);
         }
@@ -37,7 +37,14 @@ namespace app::install
 
     void LocalWorker::BufferData(void* buf, off_t offset, size_t size)
     {
-        fseeko(m_file, offset, SEEK_SET);
-        fread(buf, 1, size, m_file);
+        if (m_file != nullptr)
+        {
+            fseek(m_file, offset, SEEK_SET);
+            fread(buf, 1, size, m_file);
+        }
+        else
+        {
+            THROW_FORMAT("file is not open");
+        }
     }
 }
